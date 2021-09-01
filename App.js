adb5580237ec86ed22d53a1daf9f2939e8b60a14
Yasmine  Tillman <87497186+Yasmine011@users.noreply.github.com>
2021-08-31 22:35:43 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Dash from './dashboard';
+import Box from './box';
+import Visitors from './visitors'
+
+function App() {
+  return (
+    <div className="mainC">
+    <Dash/>
+    <Box header="Reviews" number="1,281"/>
+    <Box header="Average" number="4.6"/>
+    <Box header="Sentiment Analysis" number="960" number2="122" number3="321"/>
+    <Visitors/>
+    </div>
+  );
+}
+
+export default App;
---
+++ visitors.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Visitors() {
+    return(
+        <div className="Visitors">
+        <h1>Website Visitors</h1>
+        <p>821</p>
+        <img></img>
+        </div>
+    )
+}
+
+export default Visitors;
